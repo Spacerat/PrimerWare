cb281c16a86d71b8b01f9b0d546923eabeb5f5f6
Spacerat <spacerat3004@gmail.com>
2012-02-26 01:28:20 +0000

    Correct tick number for one second.

--- Timer.h
+++ Timer.h
@@ -1,11 +1,12 @@
 #ifndef TIMER_H_GUARD
 #define TIMER_H_GUARD
 
-#define TIME_SECOND 300
+#define TIME_SECOND 100
 
 void TIMER_initTimer(unsigned int timerNo, unsigned int timerMax);
 
 bool TIMER_checkTimer(unsigned int timerNo);
+unsigned int TIMER_ticksLeft(unsigned int timerNo);
 void TIMER_tickTimers(void);
 void TIMER_disableTimer(unsigned int timerNo);
 bool TIMER_isEnabled(unsigned int timerNo);
