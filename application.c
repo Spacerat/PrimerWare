6bb83611c3e6af721510cebac173beb26300dc9b
Spacerat <spacerat3004@gmail.com>
2012-03-07 20:47:50 +0000

    casting fail

--- application.c
+++ application.c
@@ -247,7 +247,7 @@ enum MENU_code Application_Handler(void)
 			u8 buff[PACKET_MAX_SIZE];
 			if (type == PACKET_gameWon || type == PACKET_gameFail) {
 				NET_GetPacketData(buff);
-				score += (u16 * )buff[0];
+				score += ((u16 * )buff)[0];
 				gamedata.code = (type == PACKET_gameFail ? gameStatus_Fail : gameStatus_Success);
 				//Run the game code again with the changed state.
 				//The game is responsible for checking that it is no longer supposed
